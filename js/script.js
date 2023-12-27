38c97e0c91d9b8b77ff4c9481bdac898b6569a6b
Moshiur <moshiur.robin@doer.com.bd>
2023-12-27 17:22:32 +0600

    modified

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
-// document.querySelectorAll('.dropdown-toggle').forEach(item => {
-//   item.addEventListener('click', event => {
-
-//     if(event.target.classList.contains('dropdown-toggle') ){
-//       event.target.classList.toggle('toggle-change');
-//     }
-//     else if(event.target.parentElement.classList.contains('dropdown-toggle')){
-//       event.target.parentElement.classList.toggle('toggle-change');
-//     }
-//   })
-// });
-let id = 170010;
-console.log(id);
+//custom issue list
+let issueDetails = {
+  issueId: "ISSUE-2023-11-8593",
+  issueType: "Others",
+  project: "iBAS++",
+  status: "Submitted",
+  userName: "Mohammad Wakib Hasan",
+  userMobile: "01576538723",
+  emailId: "user.email@gmail.com",
+  createdBy: "Chatbot",
+  createdOn: "12.20 PM, 24 Dec, 2023",
+  description: "This is the description of issue",
+};
+const issueId = document.getElementById("issueId");
+const issueType = document.getElementById("issueType");
+const project = document.getElementById("project");
+const status = document.getElementById("status");
+const userName = document.getElementById("userName");
+const userMobile = document.getElementById("userMobileNo");
+const emailId = document.getElementById("emailId");
+const createdBy = document.getElementById("createdBy");
+const createdOn = document.getElementById("createdOn");
+const description = document.getElementById("description");
+issueId.innerHTML = issueDetails.issueId;
+issueType.innerHTML = issueDetails.issueType;
+project.innerHTML = issueDetails.project;
+status.innerHTML = issueDetails.status;
+userName.innerHTML = issueDetails.userName;
+userMobile.innerHTML = issueDetails.userMobile;
+emailId.innerHTML = issueDetails.emailId;
+createdBy.innerHTML = issueDetails.createdBy;
+createdOn.innerHTML = issueDetails.createdOn;
+description.innerHTML = issueDetails.description;
